7213e2c77ba4f5031899bc3cc2ed135b23db26c1
tw990106 <118413280+tw990106@users.noreply.github.com>
2024-02-13 06:18:09 +0900

    first commit

--- core.js
+++ core.js
@@ -1,48 +1,65 @@
-// 랜덤번호 지정
-
-// 유저가 번호를 입력, 그리고 go라는 버튼을 누름
-
-// 버튼을 눌렀을 때, 유저가 번호를 맞히면 >> 맞혔습니다!
-
-// 랜덤번호 < 유저번호 DOWN!
-
-// 랜덤번호 > 유저번호 UP!
-
-// RESET 버튼을 누르면 게임이 리셋된다.
-
-// 5번의 기회를 다 쓰면 게임이 끝난다 (더이상 추측 불가, 버튼이 disable)
-
-// 유저가 1~100 범위 밖의 숫자를 입력하면 알려준다. 기회를 깎지 X
-
-// 유저가 이미 입력한 숫자를 또 입력하면 알려준다. 기회를 깎지 X
-
-
 let computerNum = 0;
-let playBtn = document.getElementById("playBtn"); // document = 웹페이지 그 자체, document 안에서 Id를 선택, 그 Id는 playBtn.
+let playBtn = document.getElementById("playBtn"); 
 let userInput = document.getElementById("userInput");
 let resultArea = document.getElementById("resultArea");
+let resetBtn = document.getElementById("resetBtn");
+let chance = 5; 
+let gameOver = false;
+let chanceArea = document.getElementById("chanceArea");
+let history = [];
 
-playBtn.addEventListener("click", play); // 함수를 매개변수로 넘긴 것
-
+playBtn.addEventListener("click", play); 
+resetBtn.addEventListener("click", reset);
+userInput.addEventListener("focus", function(){
+    userInput.value = "";
+})
 
 function randomNumber(){
     computerNum = Math.floor(Math.random()*100)+1;
-    // Math.random >> 랜덤한 숫자를 뽑을 수 있게 도와주는 함수. 0~1 사이의 숫자를 반환함. (소숫점으로 반환함)
-    // 이걸 1~100 사이의 숫자로 바꿔야 함. >> Math.floor 뒤의 소숫잠을 내림하여 반환함. 61.43245 >> 61, 82.5435346 >> 82 이런식으로.
     console.log("정답", computerNum);
 }
 
 function play(){
     let userValue = userInput.value;
+    
+    if(userValue<1 || userValue>100){
+        return;
+    }
+
+    if(history.includes(userValue)){
+        resultArea.textContent = "이미 입력한 숫자입니다. 다른 숫자를 입력하세요."
+        return;
+    }
+
+    chance --;
+    chanceArea.textContent = `남은 기회 : ${chance} 번`;
+    console.log("chance", chance);
+
     if(userValue < computerNum){
         resultArea.textContent = 'Up!';
     }else if(userValue > computerNum){
         resultArea.textContent = 'Down!';
     }else{
         resultArea.textContent = '정답입니다!';
+        gameOver = true;
+    }
+
+    history.push(userValue); 
+    console.log(history);
+
+    if(chance < 1){
+        gameOver=true;
+    }
+
+    if(gameOver==true){
+        playBtn.disabled = true;
     }
 }
 
+function reset(){
+    userInput.value = "";
+    randomNumber();
 
-
+    resultArea.textContent = "결과값이 여기 나옵니다";
+}
 randomNumber();
